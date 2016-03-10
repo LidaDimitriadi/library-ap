370b26fe23b62270c15afd8bfc0fcde858516495
Lida Dimitriadi <lidadimitriadi@hotmail.com>
2016-03-10 12:18:03 +0200

    lesson homework 2 completed

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,18 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  email: DS.attr('string'),
-  message: DS.attr('string')
+    email: DS.attr('string'),
+    message: DS.attr('string'),
+
+    validEmail: Ember.computed.match('email', /^.+@.+\..+$/),
+	validMessage: Ember.computed.match('message', /^.{5,1000}$/),
+
+
+ 	isValid: Ember.computed.and('validEmail', 'validMessage'),
+ 
+	isDisabled: Ember.computed.not('isValid'),
+
+	responseMessage: ''
+
+
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+    	return this.store.createRecord('contact');
+  	},
+
+  	actions: {
+
+		saveMessage(newMessage) {
+			newMessage.save().then((responce) => {
+				this.controller.set('model.responseMessage', 'We got your message and we will be in touch soon!');
+				console.log(this.controller.get('model.responseMessage'));
+				this.controller.set('model.email', '');
+				this.controller.set('model.message', '');
+			});
+		}
+	}
 });
