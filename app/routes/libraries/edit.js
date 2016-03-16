aa443a1dade4eae8e899da061d3df98a026120a9
Lida Dimitriadi <lidadimitriadi@hotmail.com>
2016-03-16 11:00:06 +0200

    completed lesson 5, added components for library and nav-link-to

--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -5,11 +5,27 @@ export default Ember.Route.extend({
     	return this.store.findRecord('library', params.library_id);
   	},
 
+  	//exw mia koinh template kai gia to new kai gia to edit th form.hbd
+  	//8etw ton titlo pou 8elw me to setupController (ftiaxnw ton virtual controller dld)
+	//kai kanw render th form.hbs 
+	
+	setupController: function(controller, model) {
+		this._super(controller, model);
+
+	    controller.set('title', 'Edit library');
+	    controller.set('buttonLabel', 'Save changes');
+	},
+
+	renderTemplate() {
+	    this.render('libraries/form');
+	},
+
   	actions: {
 
 	    saveLibrary(newLibrary) {
 	      newLibrary.save().then(() => this.transitionTo('libraries'));
 	    },
+
 
 	    //opote 8elw n alla3w route me transition, kaleitai h willTransition automata ap to ember
 	    //mporw mesa na grapsw oti 8elw kai na kanw k elegxo gia to an telika 8a ginei to transition
--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -6,6 +6,21 @@ export default Ember.Route.extend({
     	return this.store.createRecord('library');
   	},
 
+  	//exw mia koinh template kai gia to new kai gia to edit th form.hbd
+  	//8etw ton titlo pou 8elw me to setupController (ftiaxnw ton virtual controller dld)
+	//kai kanw render th form.hbs  	
+
+  	setupController: function (controller, model) {
+    	this._super(controller, model);
+
+    	controller.set('title', 'Create a new library');
+    	controller.set('buttonLabel', 'Create');
+  	},
+
+	renderTemplate() {
+	    this.render('libraries/form');
+	},
+
   	actions: {
   		saveLibrary(newLibrary) {
 			newLibrary.save().then(() => this.transitionTo('libraries'));
